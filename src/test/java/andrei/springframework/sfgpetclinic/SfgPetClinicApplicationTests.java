d51ff313cb1569786d135aa3a594b1e94cad1b1b
andrei.baltat <andrei.baltat@adswizz.com>
2021-02-14 17:42:28 +0200

    initial commit

---
+++ src/test/java/andrei/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,12 @@
+package andrei.springframework.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
